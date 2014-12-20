9c9183cec2e0fbb249e77b23f59ca4024925acc0
Paul Salaets <psalaets@gmail.com>
2014-12-20 15:32:50 -0500

    make this a bower package too

--- README.md
+++ README.md
@@ -2,20 +2,38 @@
 
 Adds find function to [localforage](http://mozilla.github.io/localForage/).
 
-## Usage
+## Setup
+
+### From npm
 
 ```
 var localforage = require('localforage');
 require('localforage-find')(localforage);
+```
 
-// using promises
+### From bower
+
+    <script>
+      localforageFind(localforage);
+    </script>
+
+## Usage
+
+Order of resultsArray is not guaranteed.
+
+### With promises
+
+```
 localforage.find(function(key, value) {
   return value.score > 20;
 }).then(function(resultsArray) {
 
 });
+```
 
-// using callbacks
+### With callbacks
+
+```
 localforage.find(function(key, value) {
   return value.score > 20;
 }, function(err, resultsArray) {
@@ -23,12 +41,16 @@ localforage.find(function(key, value) {
 });
 ```
 
-Order of resultsArray is not guaranteed.
-
 ## Install
 
 ```
 npm install localforage-find
+```
+
+or
+
+```
+bower install localforage-find
 ```
 
 ## Run Tests
@@ -38,6 +60,8 @@ You may need to delete ~/.config/browser-launcher/ first.
 ```
 npm test
 ```
+
+Also open test/scriptTag.html in a browser.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-module.exports = addFind;
+;(function(global) {
+  // commonjs environment
+  if (typeof module == 'object' && module.exports) {
+    module.exports = addFind;
+  } else { // plain old <script>
+    global.localforageFind = addFind;
+  }
 
-function addFind(localforage) {
-  localforage.find = function find(criteria, cb) {
-    cb = cb || function() {};
-    var lf = this;
+  function addFind(localforage) {
+    localforage.find = function find(criteria, cb) {
+      cb = cb || function() {};
+      var lf = this;
 
-    return lf.keys().then(function(keys) {
-      if (!keys.length) return [];
+      return lf.keys().then(function(keys) {
+        if (!keys.length) return [];
 
-      var results = [],
-          pairsSeen = 0,
-          pairsExpected = keys.length;
+        var results = [],
+            pairsSeen = 0,
+            pairsExpected = keys.length;
 
-      return lf.iterate(function(value, key) {
-        pairsSeen += 1;
+        return lf.iterate(function(value, key) {
+          pairsSeen += 1;
 
-        if (criteria(key, value)) {
-          results.push(value);
-        }
+          if (criteria(key, value)) {
+            results.push(value);
+          }
 
-        if (pairsSeen == pairsExpected) {
-          return results;
-        }
+          if (pairsSeen == pairsExpected) {
+            return results;
+          }
+        });
+      }).then(function(results) {
+        cb(null, results);
+
+        // return results here so resulting promised is fulfilled with it
+        return results;
+      }, function(err) {
+        cb(err, null);
+
+        // relaunch err so resulting promise is rejected with it
+        throw err;
       });
-    }).then(function(results) {
-      cb(null, results);
-
-      // return results here so resulting promised is fulfilled with it
-      return results;
-    }, function(err) {
-      cb(err, null);
-
-      // relaunch err so resulting promise is rejected with it
-      throw err;
-    });
-  };
-}
+    };
+  }
+})(this);
