a09019bf154cb0d1d0fb6603c734bc98f04799d7
Paul Salaets <psalaets@gmail.com>
2017-10-25 18:32:34 -0400

    rewrite cuz..?

--- test/fake-localforage.js
+++ test/fake-localforage.js
@@ -2,35 +2,41 @@
 var keys = ['a', 'b', 'c'];
 var values = ['A', 'B', 'C'];
 
-module.exports = {
-  resetFlags: function() {
-    this.breakKeys = this.breakIterate = false;
-  },
-  breakKeys: false,
-  keys: function() {
-    return new Promise(function(fulfill, reject) {
+module.exports = class FakeLocalForage {
+  constructor() {
+    this.breakKeys = false;
+    this.breakIterate = false;
+  }
+
+  resetState() {
+    this.breakKeys = false;
+    this.breakIterate = false;
+  }
+
+  keys() {
+    return new Promise((resolve, reject) => {
       if (this.breakKeys) {
         reject(new Error('breakKeys is true so keys() is broken'));
       } else {
-        fulfill(keys);
+        resolve(keys);
       }
-    }.bind(this));
-  },
-  breakIterate: false,
-  iterate: function(iterationCallback) {
-    return new Promise(function(fulfill, reject) {
+    });
+  }
+
+  iterate(iterationCallback) {
+    return new Promise((resolve, reject) => {
       if (this.breakIterate) {
         reject(new Error('breakIterate is true so iterate() is broken'));
       } else {
         for (var i = 0; i < values.length; i++) {
           var result = iterationCallback(values[i], keys[i]);
           if (result !== void 0) {
-              // make sure to return here so other fulfill() doesn't fire
-              return fulfill(result);
+              // make sure to return here so other resolve() doesn't fire
+              return resolve(result);
           }
         }
-        fulfill();
+        resolve();
       }
-    }.bind(this));
+    });
   }
-};
+}
